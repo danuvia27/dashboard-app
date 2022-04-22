4b8d559d719662d9436430ceedffec5c082aedbf
Carolina Vallecillo <danuvia27@gmail.com>
2022-04-22 14:22:40 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Button variant="contained">Start</Button>
+    </div>
+  );
+}
+
+export default App;
