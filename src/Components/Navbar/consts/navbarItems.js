d3e8bcbd0f835d01c1d0ea629f817479f7a54c96
Carolina Vallecillo <danuvia27@gmail.com>
2022-04-23 18:54:24 -0700

    Create navbarItems.js /imported icons to navbars

---
+++ src/Components/Navbar/consts/navbarItems.js
@@ -0,0 +1,58 @@
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleIcon from '@mui/icons-material/People';
+import DnsIcon from '@mui/icons-material/Dns';
+import ImageIcon from '@mui/icons-material/Image';
+import PublicIcon from '@mui/icons-material/Public';
+import BarChartIcon from '@mui/icons-material/BarChart';
+
+
+
+export const mainNavbarItems = [
+    { 
+        id:0,
+        icon: <DashboardIcon />,
+        label: 'Authentication',
+        route: 'route',
+
+    },
+    { 
+        id:1,
+        icon: <PeopleIcon />,
+        label: 'Authentication',
+        route: 'route',
+
+    },
+    { 
+        id:2,
+        icon: <DnsIcon />,
+        label: 'Authentication',
+        route: 'route',
+
+    },  { 
+        id:3,
+        icon: <ImageIcon />,
+        label: 'Authentication',
+        route: 'route',
+
+    },
+    { 
+        id:4,
+        icon: <PublicIcon />,
+        label: 'Authentication',
+        route: 'route',
+
+    },
+    { 
+        id:5,
+        icon: <BarChartIcon />,
+        label: 'Authentication',
+        route: 'route',
+
+    }
+
+
+
+
+
+
+]
